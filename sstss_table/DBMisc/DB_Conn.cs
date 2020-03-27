7e3fcc5bcc52d2dca675274a93cdfed9f1daf93c
AlphaDrgn <62599116+AlphaDrgn@users.noreply.github.com>
2020-03-28 00:27:20 +0800

    4

--- sstss_table/DBMisc/DB_Conn.cs
+++ sstss_table/DBMisc/DB_Conn.cs
@@ -1,8 +1,26 @@
 ﻿using System;
-
+using MySql.Data.MySqlClient;
 namespace DBMisc
 {
     public class DB_Conn
     {
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString = null;
+            MySqlConnection cnn ;
+			connetionString = "server=localhost;database=sstss_data;uid=root;pwd=sstssthor;";
+            cnn = new MySqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                MessageBox.Show ("Connection Open ! ");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+        }
+
     }
 }
