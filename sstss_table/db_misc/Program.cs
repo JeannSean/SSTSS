c1a3340172358eb2f2e470a15a43abaea7dc4a1e
AlphaDrgn <62599116+AlphaDrgn@users.noreply.github.com>
2020-03-28 14:12:35 +0800

    28-1

---
+++ sstss_table/db_misc/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data;
+namespace db_misc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection conn = new MySqlConnection("server=localhost;user id=root;persistsecurityinfo=True;database=sstss_data;password=sstss_thor");
+            try
+            {
+                Console.WriteLine("Connecting.........");
+                conn.Open();
+                Console.WriteLine("Connection Success!");
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Connection Failed!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
